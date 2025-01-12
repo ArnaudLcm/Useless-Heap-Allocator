9d1aac1f5d79c2d1745babce5a59f2f48a3e3279
Arnaud <contact@arnaud-lcm.cm>
2025-01-12 19:05:53 +0100

    Addign dealloc test cases

--- test/single_threaded.c
+++ test/single_threaded.c
@@ -5,17 +5,30 @@
 
 void test_too_large_chunk_alloc() { ASSERT_TRUE(alloc(1 << 29) == NULL); }
 
-void test_chunk_allocs_and_dealloc() {
+void test_valid_chunk_alloc() {
     ASSERT_TRUE(alloc(1 << 4) == heap_global.heap_start + sizeof(chunk_metadata_t));
+
+
 
     void* new_alloc = alloc(1 << 4);
     ASSERT_TRUE(new_alloc == heap_global.heap_start + sizeof(chunk_metadata_t) + (1 << 4) + sizeof(chunk_metadata_t));
 
+}
+
+void test_valid_chunk_alloc_and_dealloc() {
+
+    void* new_alloc = alloc(1 << 4);
+
     ASSERT_TRUE(dealloc(new_alloc) == 0);
+
+    ASSERT_TRUE(dealloc(new_alloc) == -1); // Check that we can't double free
+
+
 }
 
 void test_single_threaded_batch() {
     ASSERT_TRUE(alloc_init() == 0);
     test_too_large_chunk_alloc();
-    test_chunk_allocs_and_dealloc();
+    test_valid_chunk_alloc();
+    test_valid_chunk_alloc_and_dealloc();
 }
